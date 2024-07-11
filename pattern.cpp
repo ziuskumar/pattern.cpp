096c5cf2253db8105dba5ccb536835516fe04e40
zius kumar <153058154+ziuskumar@users.noreply.github.com>
2024-07-11 09:03:14 +0530

    Create pattern.cpp

---
+++ pattern.cpp
@@ -0,0 +1,142 @@
+// 111...n
+// 222...n
+// 333...n
+// nnn...n
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int  n;
+    // cout<<"Enter num : ";
+    //  cin>> n;
+//     for(int i=1; i<=n;i++){
+//        int val = i;
+//         for(int j=1; j<=n; j++){
+//             cout << val <<" ";       
+//         }
+//         cout << endl;
+//     }
+// }
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     for(int i=1; i<=4; i++){
+//         int val =i;
+//         for(int j=1; j<=4; j++){
+//             cout<< val <<" ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+//*
+//* *
+//* * *
+//* * * *
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n ;
+//      cout<<"Enter num : ";
+//      cin>> n;
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=i; j++){
+//             cout<< "* ";
+//         }
+//         cout << endl;
+//     }
+// }
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout <<"Enter N :";
+//     cin >> n;
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=i; j++){
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+//* * * *
+//* * * 
+//* * 
+//*
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n = 5;
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=(n-i+1); j++){
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+// }
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//      cout<<"Enter N: ";
+//      cin>> n;
+//         for(int i=1; i<=n; i++){
+//             int val =i;
+//             for(int j=1; j<=(n-i+1) ; j++){
+//                 cout <<"* ";
+//             }
+//             cout<<endl;
+//         }
+// }
+
+//1
+//12
+//123
+//1234
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout <<" Enter N : ";
+//     cin >> n;
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=i; j++){
+//             cout << j <<" ";
+
+//         }
+//         cout << endl;
+//     }
+// }
+
+//*****
+//*   *
+//*   *
+//*****
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n = 7;
+//     for(int i=1; i<=n; i++){
+//         cout << "*";//first row star
+//         for(int j=1; j<=n-1; j++){
+//             if(i == 1 || i == n){
+//                 cout <<"*";
+
+//             }
+//             else{
+//                 cout <<" ";
+//             }
+//         }
+//         cout<<"*"<<endl; //last row star
+//     }
+// }
